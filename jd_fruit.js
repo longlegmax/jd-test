a3750228345e539c178768e08b077333bd9871fe
longlegmax <longleg+ss@gmail.com>
2022-07-01 17:01:14 +0800

    Update jd_fruit.js

--- jd_fruit.js
+++ jd_fruit.js
@@ -68,7 +68,25 @@ async function jdFruit() {
       message = `【水果名称】${$.farmInfo.farmUserPro.name}\n`;
       console.log(`\n【已成功兑换水果】${$.farmInfo.farmUserPro.winTimes}次\n`);
       message += `【已兑换水果】${$.farmInfo.farmUserPro.winTimes}次\n`;
-      await masterHelpShare();//助力好友
+      
+      try {
+        let myShareCode = $.farmInfo.farmUserPro.shareCode
+        console.log('助力码', myShareCode)
+        await $.wait(2000)
+
+        for (let k = 0; k < 5; k++) {
+          try {
+            await runTimes(myShareCode)
+            break
+          } catch (e) {
+            console.log('runTimes Error', e)
+            await $.wait(Math.floor(Math.random() * 10 + 3) * 1000)
+          }
+        }
+      } catch (e) {
+        console.log('上报模块出错', e)
+      }
+       await masterHelpShare();//助力好友
       if ($.farmInfo.treeState === 2 || $.farmInfo.treeState === 3) {
         option['open-url'] = urlSchema;
         $.msg($.name, ``, `【京东账号${$.index}】${$.nickName || $.UserName}\n【提醒⏰】${$.farmInfo.farmUserPro.name}已可领取\n请去京东APP或微信小程序查看\n点击弹窗即达`, option);
@@ -111,7 +129,21 @@ async function jdFruit() {
   }
   await showMsg();
 }
-
+function runTimes(thisShareCode) {
+  return new Promise((resolve, reject) => {
+    $.get({
+      url: `https://api.jdsharecode.xyz/api/runTimes0701?activityId=farm&sharecode=${thisShareCode}`
+    }, (err, resp, data) => {
+      if (err) {
+        console.log('上报失败', err)
+        reject(err)
+      } else {
+        console.log(data)
+        resolve()
+      }
+    })
+  })
+}
 async function doDailyTask() {
   await taskInitForFarm();
   console.log(`开始签到`);
