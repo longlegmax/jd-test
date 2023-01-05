dd8d9f4467286fd2b6d41c508c2b8b4d0d5771de
longlegmax <longleg+ss@gmail.com>
2023-01-05 12:32:18 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -423,7 +423,9 @@ async function doHelp() {
           console.log(`${$.helpResult.data.helpShareRes.promptText}\n`)
         } else {
           console.log(`助力其他情况：${JSON.stringify($.helpResult.data.helpShareRes)}`);
-        }
+		} else {
+		  console.log(`助力好友失败: ${JSON.stringify($.helpResult)}`);
+}}}}}
 function showMsg() {
   $.log(`\n${message}\n`);
   jdNotify = $.getdata('jdPlantBeanNotify') ? $.getdata('jdPlantBeanNotify') : jdNotify;
